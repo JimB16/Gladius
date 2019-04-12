c769ea1d1994a940470cac20cf85bed2c761a4a2
JimB16 <f1@jimb.de>
2019-04-12 20:06:42 +0200

    removed the use of FileHandler

--- tools/unpack_pak.py
+++ tools/unpack_pak.py
@@ -2,24 +2,75 @@
 
 import os
 import sys
-from filehandler import FileHandler
+import struct
+import hashlib
+import gladiushashes
 
-import configuration
+
+def WriteSectionInFile(file, dirname, filename, addr, size):
+    if not os.path.exists(os.path.dirname(dirname + filename)):
+        os.makedirs(os.path.dirname(dirname + filename))
+        
+    file.seek(addr)
+    fByteArray = bytearray(file.read(size))
+    m = hashlib.md5()
+    m.update(fByteArray)
+	
+    md5 = m.hexdigest()
+    if md5 in gladiushashes.hashes:
+        filename2 = dirname + gladiushashes.hashes[md5]
+        outfilename = gladiushashes.hashes[md5]
+    else:
+        filename2 = dirname + filename
+        outfilename = filename
+	
+    if not os.path.exists(os.path.dirname(filename2)):
+        os.makedirs(os.path.dirname(filename2))
+    f = open(filename2, 'wb')
+    f.write(fByteArray)
+        
+    print "Write section in file " + filename2 + " " + m.hexdigest()
+	
+    return outfilename
+
+def ReadWord(file, Offset):
+    file.seek(Offset)
+    data = file.read(4)
+    word, = struct.unpack(">I", data)
+    return word
+
+def ReadHWord(file, Offset):
+    file.seek(Offset)
+    data = file.read(2)
+    word,  = struct.unpack(">H", data)
+    return word
+
+def ReadByte(file, Offset):
+    file.seek(Offset)
+    data = file.read(1)
+    word,  = struct.unpack("<B", data)
+    return word
+
+def ReadString0(file, address, size):
+    file.seek(address)
+    string = ""
+    bytes = bytearray(file.read(size))
+    for i in range(size):
+        if(bytes[i] != 0):
+            string += chr(bytes[i])
+        else:
+            break
+    return string
 
 
 class Win32Exe(object):
-    def __init__(self, config):
-        self.config = config
-
     def diagnose(self, filename="baserom.nds", filedir="", outFileList="", debug=False):
-        rom = FileHandler()
-        base_address = 0x0
-        rom.init(filename, base_address)
+        file = open(filename, "rb+")
 
         header_output = ""
         
         dol = BecFS()
-        header_output += dol.diagnose(rom, base_address, filedir, debug)
+        header_output += dol.diagnose(file, filedir, debug)
         
         headerfilename = filedir + outFileList
         if not os.path.exists(os.path.dirname(headerfilename)) and os.path.dirname(headerfilename):
@@ -32,10 +83,12 @@ class Win32Exe(object):
 
 
 class BecFS(object):
-    def diagnose(self, file, base_address, filedir="", debug=False):
+    def diagnose(self, file, filedir="", debug=False):
         output = ""
         
-        NrOfFiles = file.ReadHWord_r(base_address+0x4)
+        file.seek(0x4)
+        data = file.read(2)
+        NrOfFiles, = struct.unpack("<H", data)
         print "Nr of Files: " + hex(NrOfFiles)
         output += hex(NrOfFiles) + "\n"
         
@@ -44,24 +97,26 @@ class BecFS(object):
         DataMagic = []
         DataStringOffset = []
         for i in range(NrOfFiles):
-            DataStringOffset += [file.ReadWord_r(base_address+0x8+0x10*i)]
-            DataOffset += [file.ReadWord_r(base_address+0xc+0x10*i)]
-            DataSize += [file.ReadWord_r(base_address+0x10+0x10*i)]
-            DataMagic += [file.ReadWord_r(base_address+0x14+0x10*i)]
-        
+            file.seek(0x8+0x10*i)
+            data = file.read(0x10)
+            DataStringOffset_, DataOffset_, DataSize_, DataMagic_ = struct.unpack("<IIII", data)
+            DataStringOffset += [DataStringOffset_]
+            DataOffset += [DataOffset_]
+            DataSize += [DataSize_]
+            DataMagic += [DataMagic_]
+		
         filename = ""
         for i in range(NrOfFiles): # NrOfFiles
-            filename = file.ReadString0(DataStringOffset[i], 0x20)
-            file.WriteSectionInFile(filedir + filename, DataOffset[i], DataSize[i])
-            print filename
-            output += filename + " " + hex(DataOffset[i]) + " " + hex(DataSize[i]) + " " + hex(DataMagic[i]) + " " + hex(DataStringOffset[i]) + "\n"
+            filename = ReadString0(file, DataStringOffset[i], 0x20)
+            filename2 = WriteSectionInFile(file, filedir, filename, DataOffset[i], DataSize[i])
+            print filename2
+            output += filename2 + " " + hex(DataOffset[i]) + " " + hex(DataSize[i]) + " " + hex(DataMagic[i]) + " " + hex(DataStringOffset[i]) + "\n"
         
         return output
 
         
 if __name__ == "__main__":
-    conf = configuration.Config()
-    dvd = Win32Exe(conf)
+    dvd = Win32Exe()
     
     filename = ""
     outdir = ""
