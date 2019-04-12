7c0d2deb23a3fff7ff112f62537411cd317aef61
JimB16 <f1@jimb.de>
2019-04-12 14:49:28 +0200

    fixed bec tools and added hashes to identify files

--- tools/create_bec.py
+++ tools/create_bec.py
@@ -7,7 +7,7 @@ import operator
 
 
 class RomSection():
-    def __init__(self, name, name2, hash, address, size2, flags, size, checksum):
+    def __init__(self, name, name2, hash, address, size2, flags, size, checksum, checksum2):
         self.name = name
         self.name2 = name2
         self.hash = hash
@@ -16,19 +16,13 @@ class RomSection():
         self.flags = flags
         self.size = size
         self.size2 = size2
-        self.new_size = size
         self.checksum = checksum
+        self.checksum2 = checksum2
 
 RomMap = []
 
-def addRomSection(name, name2, hash, address, size2, flags, size, checksum): # filename, hash?, offset, offsetcorrection, flags?, filesize, checksum?
-    RomMap.append(RomSection(name, name2, hash, address, size2, flags, size, checksum))
-
-def changeAddressOfRomSection(old_addr, new_addr, old_size, new_size):
-    for item in RomMap:
-        if ((item.flags != 0) and (item.address == old_addr) and (item.size == old_size)):
-            item.address = new_addr
-            item.size = new_size
+def addRomSection(name, name2, hash, address, size2, flags, size, checksum, checksum2): # filename, hash?, offset, offsetcorrection, flags?, filesize, checksum?
+    RomMap.append(RomSection(name, name2, hash, address, size2, flags, size, checksum, checksum2))
 
 def alignFileSizeWithZeros(file, pos, alignment):
     target = (pos + alignment - 1) & (0x100000000-alignment)
@@ -45,12 +39,12 @@ def createBecArchive(dir, filename, becmap, gc, debug=False):
     with open(becmap) as fin:
         for line in fin:
             words = line.split() # filename, ?, offset, flags?, filesize
-            if len(words) == 8:
+            if len(words) == 9:
                 FileSize = os.path.getsize(dir + "/" + words[0])
                 FileSize2 = 0
                 if words[1] != "nothing":
                     FileSize2 = os.path.getsize(dir + "/" + words[1])
-                addRomSection(words[0], words[1], int(words[2], 16), int(words[3], 16), FileSize2, int(words[5], 16), FileSize, int(words[7], 16)) # filename, filename2, hash?, offset, filesize2, flags?, filesize, checksum?
+                addRomSection(words[0], words[1], int(words[2], 16), int(words[3], 16), FileSize2, int(words[5], 16), FileSize, int(words[7], 16), int(words[8], 16)) # filename, filename2, hash?, offset, filesize2, flags?, filesize, checksum?, checksum2?
             elif len(words) == 3:
                 filealignment = int(words[0], 16)
                 NrOfFiles = int(words[1], 16)
@@ -69,33 +63,29 @@ def createBecArchive(dir, filename, becmap, gc, debug=False):
     
     # updated the filesizes
     RomMap.sort(key=operator.attrgetter('address')) # address
-    startaddress = RomMap[0].address
-    addr = startaddress
+    addr = RomMap[0].address
     diffaddr = 0
     oldaddr = addr
-    itemnr = 0
+	
     for item in RomMap:
         if item.flags != 0:
-            #changeAddressOfRomSection(item.address, addr, item.size, size)
             continue
         
-        size = os.path.getsize(dir + "/" + item.name)
+        #item.size = os.path.getsize(dir + "/" + item.name)
         if item.address != addr and diffaddr == 0:
             print("Adr diff: org: " + hex(item.address) + ", new: " + hex(addr) + ", prevaddr: " + hex(oldaddr))
             diffaddr = 1
         oldaddr = addr
         item.new_address = addr
-        item.new_size = size
-        if item.size2 != 0:
+        if item.size2 > 0:
             addr += item.size2 + 8 + (filealignment - 1)
             addr &= (0x100000000 - filealignment)
-        addr += size + (filealignment - 1)
-        if gc == 1:
+        addr += item.size + (filealignment - 1)
+        if ((item.size2 == 0) and (item.size > 0)) or (gc == 1):
             addr += 8 # GCExclusive + 8 for checksum being saved here
         addr &= (0x100000000 - filealignment)
-        if (item.size2 == 0) and (size == 0) and (gc == 0): # PS2Exclusive
+        if (item.size2 == 0) and (item.size == 0) and (gc == 0): # PS2Exclusive
             addr += filealignment
-        itemnr += 1
 
     # fix the second instant of some file entries
     for i in range(len(RomMap)):
@@ -103,18 +93,15 @@ def createBecArchive(dir, filename, becmap, gc, debug=False):
             for j in range(len(RomMap)):
                 if (i != j) and (RomMap[j].flags == 0) and (RomMap[i].address == RomMap[j].address):
                     RomMap[i].new_address = RomMap[j].new_address
+                    RomMap[i].size = RomMap[j].size
     
     RomMap.sort(key=operator.attrgetter('hash')) # address
     for item in RomMap:
         output_rom.write(struct.pack('<I', int(item.hash)))
         output_rom.write(struct.pack('<I', int(item.new_address)))
-        #output_rom.write(struct.pack('<I', int(item.address)))
         output_rom.write(struct.pack('<B', int((item.size2 >> 0) & 0xff)))
         output_rom.write(struct.pack('<B', int((item.size2 >> 8) & 0xff)))
         output_rom.write(struct.pack('<B', int((item.size2 >> 16) & 0xff)))
-        #output_rom.write(struct.pack('<B', int(0)))
-        #output_rom.write(struct.pack('<B', int(0)))
-        #output_rom.write(struct.pack('<B', int(0)))
         output_rom.write(struct.pack('<B', int(item.flags)))
         output_rom.write(struct.pack('<I', int(item.size)))
 
@@ -125,22 +112,20 @@ def createBecArchive(dir, filename, becmap, gc, debug=False):
     for item in RomMap:
         if item.flags != 0: # skip files where flag == 2
             continue
-        if item.size2 != 0:
+
+        if item.size2 > 0:
             filepath2 = dir + "/" + item.name2
             filedata2 = bytearray(open(filepath2, "rb").read())
             output_rom.write(filedata2)
-            if (gc == 0):
-                output_rom.write(struct.pack('<I', int(0))) # checksum? PS2Exclusive
-                output_rom.write(struct.pack('<I', int(0))) # PS2Exclusive
+            output_rom.write(struct.pack('<II', int(item.checksum), int(item.checksum2))) # checksum? PS2Exclusive
             alignFileSizeWithZeros(output_rom, output_rom.tell(), filealignment)
 		
         filepath = dir + "/" + item.name
         #print "writing " + dir + "/" + item.name
         filedata = bytearray(open(filepath, "rb").read())
         output_rom.write(filedata)
-        if gc == 1:
-            output_rom.write(struct.pack('<I', item.checksum)) # checksum? GCExclusive
-            output_rom.write(struct.pack('<I', int(0))) # GCExclusive
+        if ((item.size2 == 0) and (item.size > 0)) or (gc == 1):
+            output_rom.write(struct.pack('<II', item.checksum, item.checksum2)) # checksum? GCExclusive
 		
         alignFileSizeWithZeros(output_rom, output_rom.tell(), filealignment)
 			
@@ -166,10 +151,10 @@ if __name__ == "__main__":
         if sys.argv[i] == "-gc": # switch between PS2 and GC Mode, the bec-formats they use don't seem completely compatible
             gc = 1
             i += 1
-        if sys.argv[i] == "-dir":
+        elif sys.argv[i] == "-dir":
             dir = sys.argv[i+1]
             i += 2
-        if sys.argv[i] == "-becmap":
+        elif sys.argv[i] == "-becmap":
             becmap = sys.argv[i+1]
             i += 2
         elif sys.argv[i] == "-o":
--- tools/unpack_bec.py
+++ tools/unpack_bec.py
@@ -2,203 +2,240 @@
 
 import os
 import sys
-from filehandler import FileHandler
+import struct
+import hashlib
+import gladiushashes
 
-import configuration
 
-
-class Win32Exe(object):
-    def __init__(self, config):
-        self.config = config
-
-    def diagnose(self, filename="baserom.nds", filedir="", outFileList="", debug=False):
-        rom = FileHandler()
-        base_address = 0x0
-        rom.init(filename, base_address)
-
-        header_output = ""
+def WriteSectionInFile(file, dirname, filename, addr, size):
+    if not os.path.exists(os.path.dirname(dirname + filename)):
+        os.makedirs(os.path.dirname(dirname + filename))
         
-        dol = BecFS()
-        header_output += dol.diagnose(rom, base_address, filedir, debug)
+    file.seek(addr)
+    fByteArray = bytearray(file.read(size))
+    m = hashlib.md5()
+    m.update(fByteArray)
+	
+    md5 = m.hexdigest()
+    if md5 in gladiushashes.hashes:
+        filename2 = dirname + gladiushashes.hashes[md5]
+        outfilename = gladiushashes.hashes[md5]
+    else:
+        filename2 = dirname + filename
+        outfilename = filename
+	
+    if not os.path.exists(os.path.dirname(filename2)):
+        os.makedirs(os.path.dirname(filename2))
+    f = open(filename2, 'wb')
+    f.write(fByteArray)
         
-        headerfilename = filedir + outFileList
-        if not os.path.exists(os.path.dirname(headerfilename)) and os.path.dirname(headerfilename):
-            os.makedirs(os.path.dirname(headerfilename))
-        fheader = open(headerfilename, 'w')
-        fheader.write(header_output)
-        
-        output = ""
-        return (output)
+    print("Write section in file " + filename2 + " " + m.hexdigest())
+	
+    return outfilename
+
+def ReadWord(file, Offset):
+    file.seek(Offset)
+    data = file.read(4)
+    word, = struct.unpack(">I", data)
+    return word
+
+def ReadHWord(file, Offset):
+    file.seek(Offset)
+    data = file.read(2)
+    word,  = struct.unpack(">H", data)
+    return word
+
+def ReadByte(file, Offset):
+    file.seek(Offset)
+    data = file.read(1)
+    word,  = struct.unpack("<B", data)
+    return word
 
 
-class BecFS(object):
-    def diagnose(self, file, base_address, filedir="", debug=False):
-        output = ""
-        
-        FileAlignment = file.ReadHWord_r(base_address+0x6)
-        NrOfFiles = file.ReadWord_r(base_address+0x8)
-        print "Nr of Files: " + hex(NrOfFiles)
-        HeaderMagic = file.ReadWord_r(base_address+0xC)
-        output += hex(FileAlignment) + " " + hex(NrOfFiles) + " " + hex(HeaderMagic) + "\n"
-        
-        DataPos = []
-        DataOffset = []
-        OffsetCorrection = []
-        Data3 = []
-        DataSize = []
-        CheckSum = []
-        for i in range(NrOfFiles):
-            DataPos += [file.ReadWord_r(base_address+0x10+0x10*i)]
-            DataOffset += [file.ReadWord_r(base_address+0x14+0x10*i)]
-            correction = file.ReadByte(base_address+0x1a+0x10*i) << 16
-            correction += file.ReadByte(base_address+0x19+0x10*i) << 8
-            correction += file.ReadByte(base_address+0x18+0x10*i) << 0
-            OffsetCorrection += [correction]
-            Data3 += [file.ReadByte(base_address+0x1b+0x10*i)] # 0 or 2
-            DataSize += [file.ReadWord_r(base_address+0x1c+0x10*i)]
-            Offset = DataOffset[i]
-            Offset += OffsetCorrection[i]
-            Offset += FileAlignment - 1
+def diagnose(filename="baseiso.iso", filedir="", outFileList="", debug=False):
+    file = open(filename, "rb+")
+
+    header_output = ""
+    header_output += diagnose2(file, filedir, debug)
+    
+    headerfilename = filedir + outFileList
+    if not os.path.exists(os.path.dirname(headerfilename)) and os.path.dirname(headerfilename):
+        os.makedirs(os.path.dirname(headerfilename))
+    fheader = open(headerfilename, 'w')
+    fheader.write(header_output)
+
+def diagnose2(file2, filedir="", debug=False):
+    output = ""
+    
+    file2.seek(0x6)
+    data = file2.read(10)
+    FileAlignment, NrOfFiles, HeaderMagic = struct.unpack("<HII", data)
+    print("Nr of Files: " + str(NrOfFiles))
+    output += hex(FileAlignment) + " " + hex(NrOfFiles) + " " + hex(HeaderMagic) + "\n"
+    
+    DataPos = []
+    DataOffset = []
+    OffsetCorrection = []
+    Data3 = []
+    DataSize = []
+    CheckSums1 = []
+    CheckSums2 = []
+    for i in range(NrOfFiles):
+        #file2.seek(0x10+0x10*i)
+        data = file2.read(0x10)
+        DataPos_, DataOffset_, correction0, correction1, correction2, Data3_, DataSize_ = struct.unpack("<IIBBBBI", data)
+
+        DataPos += [DataPos_]
+        DataOffset += [DataOffset_]
+        correction = (correction2 << 16) + (correction1 << 8) + (correction0 << 0)
+        OffsetCorrection += [correction]
+        Data3 += [Data3_] # 0 or 2
+        DataSize += [DataSize_]
+	
+    for i in range(NrOfFiles):
+        if OffsetCorrection[i] > 0: # PS2 version (checksum after zlib file)
+            Offset = DataOffset[i] + OffsetCorrection[i]
+        else: # GC version (checksum after uncompressed file)
+            Offset = DataOffset[i] + (FileAlignment - 1)
             Offset &= (0x100000000 - FileAlignment)
-            CheckSum += [file.ReadWord_r(Offset + DataSize[i])]
+            Offset += DataSize[i]
+        file2.seek(Offset)
+        data = file2.read(8)
+        CheckSum1, CheckSum2 = struct.unpack("<II", data)
+        CheckSums1 += [CheckSum1]
+        CheckSums2 += [CheckSum2]
+    
+    filename = ""
+    for i in range(NrOfFiles): # NrOfFiles
+        Offset = DataOffset[i] + OffsetCorrection[i] + (FileAlignment - 1)
+        if OffsetCorrection[i] > 0:
+            Offset += 8
+        Offset &= (0x100000000 - FileAlignment)
+
+        if ReadHWord(file2, Offset) == 0x2f2f:
+            filename = str(i) + ".txt"
+        elif (ReadWord(file2, Offset) == 0x0D0A2F2F):
+            filename = str(i) + ".txt"
+        elif (ReadWord(file2, Offset) == 0x0D0A0D0A) and (ReadHWord(file2, Offset+4) == 0x2F2F):
+            filename = str(i) + ".txt"
+        elif (ReadWord(file2, Offset) == 0x436F7079) and (ReadWord(file2, Offset+4) == 0x72696768):
+            filename = str(i) + ".txt"
+        elif (ReadWord(file2, Offset) == 0x56455253):
+            filename = str(i) + ".vers"
+        elif (ReadWord(file2, Offset) == 0x50545450):
+            filename = str(i) + ".pttp"
+        elif (ReadWord(file2, Offset) == 0x50414B31):
+            filename = str(i) + ".pak1"
+        elif (ReadWord(file2, Offset) == 0x23233832) and (ReadWord(file2, Offset+4) == 0x32300D0A):
+            filename = str(i) + ".txt"
+        elif (ReadWord(file2, Offset) == 0x1D200000):
+            filename = str(i) + ".bin"
+        elif (ReadWord(file2, Offset) == 0x504D5332):
+            filename = str(i) + ".pms2"
+        elif (ReadWord(file2, Offset) == 0x40656368) and (ReadWord(file2, Offset+4) == 0x6F206F66):
+            filename = str(i) + ".bat"
+        elif (ReadWord(file2, Offset) == 0x504F5309):
+            filename = str(i) + ".pos"
+        elif (ReadWord(file2, Offset) == 0x0D0A6675):
+            filename = str(i) + ".txt"
+        elif (ReadWord(file2, Offset) == 0x4C4F4341) and (ReadWord(file2, Offset+4) == 0x544F5253):
+            filename = str(i) + ".locators"
+        elif (ReadWord(file2, Offset) == 0x312C2242) and (ReadWord(file2, Offset+4) == 0x6174746C):
+            filename = str(i) + ".txt"
+        elif (ReadWord(file2, Offset) == 0x66756E63) and (ReadWord(file2, Offset+4) == 0x74696F6E):
+            filename = str(i) + ".txt"
+        elif (ReadWord(file2, Offset) == 0x52454D20) and (ReadWord(file2, Offset+4) == 0x2D2D2047):
+            filename = str(i) + ".bat"
+        elif (ReadWord(file2, Offset) == 0x53554254) and (ReadWord(file2, Offset+4) == 0x49544C45):
+            filename = str(i) + ".subs.txt"
+        elif (ReadWord(file2, Offset) == 0x53474F44):
+            filename = str(i) + ".sgod"
+        elif (ReadWord(file2, Offset) == 0x0D0A0D0A):
+            filename = str(i) + ".txt"
+        elif (ReadWord(file2, Offset) == 0x4E554D45) and (ReadWord(file2, Offset+4) == 0x44474553):
+            filename = str(i) + ".txt"
+        elif (ReadWord(file2, Offset) == 0x4E554D43) and (ReadWord(file2, Offset+4) == 0x52454449):
+            filename = str(i) + ".txt"
+        elif (ReadWord(file2, Offset) == 0x01000000):
+            filename = str(i) + ".bin"
+        elif (ReadWord(file2, Offset) == 0x02000000):
+            filename = str(i) + ".bin"
+        elif (ReadWord(file2, Offset) == 0x4E414D45):
+            filename = str(i) + ".txt"
+        elif (ReadWord(file2, Offset) == 0x200D0A2F):
+            filename = str(i) + ".txt"
+        elif (ReadWord(file2, Offset) == 0x50415448):
+            filename = str(i) + ".bat"
+        elif (ReadWord(file2, Offset) == 0x0D0A4E41):
+            filename = str(i) + ".txt"
+        elif (ReadHWord(file2, Offset) == 0x2E2E) and (ReadByte(file2, Offset+2) == 0x5C):
+            filename = str(i) + ".txt"
+        elif (ReadWord(file2, Offset) == 0x54494E54) and (ReadWord(file2, Offset+4) == 0x494E473A):
+            filename = str(i) + ".tinting.txt"
+        elif (ReadWord(file2, Offset) == 0x0D0A2066):
+            filename = str(i) + ".txt"
+        elif (ReadWord(file2, Offset) == 0x4D4F4449) and (ReadWord(file2, Offset+4) == 0x54454D53):
+            filename = str(i) + ".txt"
+        elif (ReadWord(file2, Offset) == 0x5343454E) and (ReadWord(file2, Offset+4) == 0x453A0909):
+            filename = str(i) + ".txt"
+        elif (ReadWord(file2, Offset) == 0x496E7465) and (ReadWord(file2, Offset+4) == 0x72666163):
+            filename = str(i) + ".txt"
+        elif (ReadWord(file2, Offset) == 0x4C656167) and (ReadWord(file2, Offset+4) == 0x75655374):
+            filename = str(i) + ".txt"
+        elif (ReadWord(file2, Offset) == 0x5C70726F) and (ReadWord(file2, Offset+4) == 0x6A656374):
+            filename = str(i) + ".txt"
+        elif (ReadHWord(file2, Offset) == 0x0D0A):
+            filename = str(i) + ".txt"
+        elif (ReadWord(file2, Offset) == 0x5041583A):
+            filename = str(i) + ".txt"
+        elif (ReadWord(file2, Offset) == 0x3A204765):
+            filename = str(i) + ".bat"
+        elif (ReadWord(file2, Offset) == 0x6275696C) and (ReadWord(file2, Offset+4) == 0x6470616B):
+            filename = str(i) + ".txt"
+        elif (ReadWord(file2, Offset) == 0x5363686F) and (ReadWord(file2, Offset+4) == 0x6F6C5374):
+            filename = str(i) + ".txt"
+        elif (ReadWord(file2, Offset) == 0x40000000):
+            filename = str(i) + ".mih"
+        else:
+            filename = str(i) + ".bin"
+		
+        filename = WriteSectionInFile(file2, filedir, filename, Offset, DataSize[i])
+		
+        filename2 = "zlib/" + filename + ".zlib"
+        if OffsetCorrection[i] > 0:
+            filename2 = WriteSectionInFile(file2, filedir, filename2, DataOffset[i], OffsetCorrection[i])
+        else:
+            filename2 = "nothing"
+	
+        output += filename + " " + filename2 + " " + hex(DataPos[i]) + " " + hex(DataOffset[i]) + " " + hex(OffsetCorrection[i]) + " " + hex(Data3[i]) + " " + hex(DataSize[i]) + " " + hex(CheckSums1[i]) + " " + hex(CheckSums2[i]) + "\n"
         
-        filename = ""
-        for i in range(NrOfFiles): # NrOfFiles
-            Offset = DataOffset[i]
-            Offset += OffsetCorrection[i]
-            Offset += FileAlignment - 1
-            Offset &= (0x100000000 - FileAlignment)
-            if file.ReadHWord(Offset) == 0x2f2f:
-                filename = str(i) + ".txt"
-            elif (file.ReadWord(Offset) == 0x0D0A2F2F):
-                filename = str(i) + ".txt"
-            elif (file.ReadWord(Offset) == 0x0D0A0D0A) and (file.ReadHWord(Offset+4) == 0x2F2F):
-                filename = str(i) + ".txt"
-            elif (file.ReadWord(Offset) == 0x436F7079) and (file.ReadWord(Offset+4) == 0x72696768):
-                filename = str(i) + ".txt"
-            elif (file.ReadWord(Offset) == 0x56455253):
-                filename = str(i) + ".vers"
-            elif (file.ReadWord(Offset) == 0x50545450):
-                filename = str(i) + ".pttp"
-            elif (file.ReadWord(Offset) == 0x50414B31):
-                filename = str(i) + ".pak1"
-            elif (file.ReadWord(Offset) == 0x23233832) and (file.ReadWord(Offset+4) == 0x32300D0A):
-                filename = str(i) + ".txt"
-            elif (file.ReadWord(Offset) == 0x1D200000):
-                filename = str(i) + ".bin"
-            elif (file.ReadWord(Offset) == 0x504D5332):
-                filename = str(i) + ".pms2"
-            elif (file.ReadWord(Offset) == 0x40656368) and (file.ReadWord(Offset+4) == 0x6F206F66):
-                filename = str(i) + ".bat"
-            elif (file.ReadWord(Offset) == 0x504F5309):
-                filename = str(i) + ".pos"
-            elif (file.ReadWord(Offset) == 0x0D0A6675):
-                filename = str(i) + ".txt"
-            elif (file.ReadWord(Offset) == 0x4C4F4341) and (file.ReadWord(Offset+4) == 0x544F5253):
-                filename = str(i) + ".locators"
-            elif (file.ReadWord(Offset) == 0x312C2242) and (file.ReadWord(Offset+4) == 0x6174746C):
-                filename = str(i) + ".txt"
-            elif (file.ReadWord(Offset) == 0x66756E63) and (file.ReadWord(Offset+4) == 0x74696F6E):
-                filename = str(i) + ".txt"
-            elif (file.ReadWord(Offset) == 0x52454D20) and (file.ReadWord(Offset+4) == 0x2D2D2047):
-                filename = str(i) + ".bat"
-            elif (file.ReadWord(Offset) == 0x53554254) and (file.ReadWord(Offset+4) == 0x49544C45):
-                filename = str(i) + ".subs.txt"
-            elif (file.ReadWord(Offset) == 0x53474F44):
-                filename = str(i) + ".sgod"
-            elif (file.ReadWord(Offset) == 0x0D0A0D0A):
-                filename = str(i) + ".txt"
-            elif (file.ReadWord(Offset) == 0x4E554D45) and (file.ReadWord(Offset+4) == 0x44474553):
-                filename = str(i) + ".txt"
-            elif (file.ReadWord(Offset) == 0x4E554D43) and (file.ReadWord(Offset+4) == 0x52454449):
-                filename = str(i) + ".txt"
-            elif (file.ReadWord(Offset) == 0x01000000):
-                filename = str(i) + ".bin"
-            elif (file.ReadWord(Offset) == 0x02000000):
-                filename = str(i) + ".bin"
-            elif (file.ReadWord(Offset) == 0x4E414D45):
-                filename = str(i) + ".txt"
-            elif (file.ReadWord(Offset) == 0x200D0A2F):
-                filename = str(i) + ".txt"
-            elif (file.ReadWord(Offset) == 0x50415448):
-                filename = str(i) + ".bat"
-            elif (file.ReadWord(Offset) == 0x0D0A4E41):
-                filename = str(i) + ".txt"
-            elif (file.ReadHWord(Offset) == 0x2E2E) and (file.ReadByte(Offset+2) == 0x5C):
-                filename = str(i) + ".txt"
-            elif (file.ReadWord(Offset) == 0x54494E54) and (file.ReadWord(Offset+4) == 0x494E473A):
-                filename = str(i) + ".tinting.txt"
-            elif (file.ReadWord(Offset) == 0x0D0A2066):
-                filename = str(i) + ".txt"
-            elif (file.ReadWord(Offset) == 0x4D4F4449) and (file.ReadWord(Offset+4) == 0x54454D53):
-                filename = str(i) + ".txt"
-            elif (file.ReadWord(Offset) == 0x5343454E) and (file.ReadWord(Offset+4) == 0x453A0909):
-                filename = str(i) + ".txt"
-            elif (file.ReadWord(Offset) == 0x496E7465) and (file.ReadWord(Offset+4) == 0x72666163):
-                filename = str(i) + ".txt"
-            elif (file.ReadWord(Offset) == 0x4C656167) and (file.ReadWord(Offset+4) == 0x75655374):
-                filename = str(i) + ".txt"
-            elif (file.ReadWord(Offset) == 0x5C70726F) and (file.ReadWord(Offset+4) == 0x6A656374):
-                filename = str(i) + ".txt"
-            elif (file.ReadHWord(Offset) == 0x0D0A):
-                filename = str(i) + ".txt"
-            elif (file.ReadWord(Offset) == 0x5041583A):
-                filename = str(i) + ".txt"
-            elif (file.ReadWord(Offset) == 0x3A204765):
-                filename = str(i) + ".bat"
-            elif (file.ReadWord(Offset) == 0x6275696C) and (file.ReadWord(Offset+4) == 0x6470616B):
-                filename = str(i) + ".txt"
-            elif (file.ReadWord(Offset) == 0x5363686F) and (file.ReadWord(Offset+4) == 0x6F6C5374):
-                filename = str(i) + ".txt"
-            else:
-                filename = str(i) + ".bin"
-			
-            file.WriteSectionInFile(filedir + filename, Offset, DataSize[i])
-			
-            filename2 = "zlib/" + filename + ".zlib"
-            if OffsetCorrection[i] > 0:
-                file.WriteSectionInFile(filedir + filename2, DataOffset[i], OffsetCorrection[i])
-            else:
-                filename2 = "nothing"
-			
-            output += filename + " " + filename2 + " " + hex(DataPos[i]) + " " + hex(DataOffset[i]) + " " + hex(OffsetCorrection[i]) + " " + hex(Data3[i]) + " " + hex(DataSize[i]) + " " + hex(CheckSum[i]) + "\n"
-        
-        return output
+    return output
 
         
 if __name__ == "__main__":
-    conf = configuration.Config()
-    dvd = Win32Exe(conf)
-    
+    HelpText = "usage: unpack_bec.py -i InputFile -o OutputFolder -filelist OutputFileList"
     filename = ""
     outdir = ""
-    output = ""
-    outFileList = ""
+    outFileList = "bec_info.txt"
     
     i = 1
+    if (len(sys.argv) != 7) or (len(sys.argv) != 7):
+        print("Error: wrong number of arguments")
+        print(HelpText)
+        sys.exit()
     while i < len(sys.argv):
-        if sys.argv[i] == "-x":
-            cmd = "unpack"
+        if sys.argv[i] == "-help":
+            print(HelpText)
+            sys.exit()
+        elif sys.argv[i] == "-i":
             filename = sys.argv[i+1]
-            outdir = os.path.splitext(filename)[0]
             i += 2
-        elif sys.argv[i] == "-d":
-            cmd = "diagnose"
-            filename = sys.argv[i+1]
-            outdir = os.path.dirname(filename)
-            outFileList = "bec_info.txt"
-            i += 2
-        elif sys.argv[i] == "-of":
+        elif sys.argv[i] == "-o":
             outdir = sys.argv[i+1]
             i += 2
         elif sys.argv[i] == "-filelist":
             outFileList = sys.argv[i+1]
             i += 2
-    
-    print(cmd + ': ' + filename)
-    
-    if cmd == "diagnose":
-        print(cmd + ' to ' + outFileList)
-        output = dvd.diagnose(filename, outdir, outFileList)
-    
-    print output
+
+    print('unpacking ' + filename + "...")
+    diagnose(filename, outdir, outFileList)
